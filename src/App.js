4cf84e3c3d4ee5f5b97eaa5c7dd74f46ef684340
Elijah Ohanya <ohanyaelijah8@gmail.com>
2023-07-25 05:03:28 -0700

    first commited file on react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      
+      <nav className='navbar'>
+
+        <div className='login'>
+          <h1>Login</h1>
+        </div>  
+        
+          <div className='ha'>
+            <h2>Home</h2>
+            <h2>About</h2>
+          </div>
+      
+      </nav>
+
+      <main>
+        
+      </main>
+      <footer>
+        <h3>
+          Elijah Ohanya application @ 2023
+        </h3>
+      </footer>
     </div>
   );
 }
