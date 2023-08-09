a61ebfb5a8037f26054efc0a43a0f97cb5524152
Elijah Ohanya <ohanyaelijah8@gmail.com>
2023-08-09 09:30:29 -0700

    okay

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,12 +1,13 @@
+
 import React from 'react'
 
-const Header = ( prop ) => {
-    
+const Header = () => {
   return (
-      <nav style={{ minHeight: "10vh", backgroundColor: 'blue',color:'white',fontWeight:'bolder',textTransform:'capitalize',padding:'10px',letterSpacing:'2px' }}>
-          
-          <h1 >{prop.appname }</h1>
-      </nav>
+      <div style={{backgroundColor:'blue',color:'white'}}>
+          <h1>
+              Groceries List
+          </h1>
+    </div>
   )
 }
 
