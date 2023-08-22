1e57f4f2deff547f436f923afb40fd6e19219816
Elijah Ohanya <ohanyaelijah8@gmail.com>
2023-08-22 05:21:21 -0700

    class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-
 const App = () => {
- 
+  class Player {
+    constructor(id, name) {
+      this.id = id;
+      this.name = name;
+    }
+  }
+
+  const harlaan = {
+    id: "4",
+    name: "harlaan",
+  };
+
+  const messi = new Player(1, "messi");
+  const ronaldo = new Player(2, "ronaldo");
+  const neymar = new Player(3, "neymar");
+  const db = [messi, ronaldo, neymar, harlaan];
+  console.log(db);
   return (
-    <div>welcome</div>
+    <ul>
+      {
+        db.map(obj => {
+          return (
+            <li key={obj.id}><b>
+              {
+                obj.name
+              }
+            </b><hr/>
+            </li>
+          )
+        })
+      }
+    </ul>
   );
 };
-
 export default App;
