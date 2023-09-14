6080b1c4c2154a0403377a6c744590fa4a00a2e0
Elijah Ohanya <ohanyaelijah8@gmail.com>
2023-09-14 07:34:25 -0700

    okay

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
+import React from 'react'
+
 const App = () => {
-  class Player {
-    constructor(id, name) {
-      this.id = id;
-      this.name = name;
-    }
-  }
-
-  const harlaan = {
-    id: "4",
-    name: "harlaan",
-  };
-
-  const messi = new Player(1, "messi");
-  const ronaldo = new Player(2, "ronaldo");
-  const neymar = new Player(3, "neymar");
-  const db = [messi, ronaldo, neymar, harlaan];
-  console.log(db);
   return (
-    <ul>
-      {
-        db.map(obj => {
-          return (
-            <li key={obj.id}><b>
-              {
-                obj.name
-              }
-            </b><hr/>
-            </li>
-          )
-        })
-      }
-    </ul>
-  );
-};
-export default App;
+    <div className='app' style={{minHeight:'100vh',backgroundColor:'blue'}}>
+
+    </div>
+  )
+}
+
+export default App
