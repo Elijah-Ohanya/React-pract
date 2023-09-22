6a99b3435d21812946cdd168440c54e84b67b063
Elijah Ohanya <ohanyaelijah8@gmail.com>
2023-09-22 16:22:14 -0700

    okay lets continue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import Header from './Components/Header'
+import Main from './Components/Main'
+import Footer from './Components/Footer'
 
 const App = () => {
-  return (
-    <div className='app' style={{minHeight:'100vh',backgroundColor:'blue'}}>
+  const h1 = 'Groceries List'
+  const h3 = 'Copyright @ 2023'
+  const listofobj = [
+    {
+      id: 1,
+      name:'tele'
+    },
+    {
+      id: 2,
+      name:'chair'
+    },
+    {
+      id: 3,
+      name:'table'
+    }
 
+  ]
+  return (
+    <div className='App'>
+      <Header header={h1} />
+      <Main names={listofobj} />
+      <Footer footer={h3} />
     </div>
   )
 }
