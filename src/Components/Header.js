b9da7ca0ac0fa6d6c02a646ee03a814d0bc8e89e
Elijah Ohanya <ohanyaelijah8@gmail.com>
2023-10-15 07:59:20 -0700

    first commit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,17 +1,1 @@
-import React from 'react'
 
-const Header = () => {
-  return(
-    <header>
-      <h1>Groceries-List</h1>
-      
-      <h3>Home</h3>
-      <h3>Contact</h3>
-      <h3>About</h3>
-      
-      <h1>Login</h1>
-      </header>
-  )
-}
-
-export default Header
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,19 +1,1 @@
-import React from 'react'
 
-const Main = (prop) => {
-  return (
-    <main>
-      {
-        prop.list.map(obj => {
-          return (
-            <h1 key={obj.id}>
-              {obj.name}
-            </h1>
-          )
-        })
-      }
-     </main>
-  )
-}
-
-export default Main;
