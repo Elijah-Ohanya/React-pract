140a1ec04b10c4dcb653aeee70283cca7542ee03
Elijah Ohanya <ohanyaelijah8@gmail.com>
2023-10-17 22:23:28 -0700

    forth commited file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import Header from './Components/Header';
 import Main from './Components/Main';
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
 
+registerLink.addEventListener('click', () => {
+  wrapper.classList.add('active');
+});
 const App = () => {
   return (
     <div className='body'>
      <Header/>
-      <Main />
+      <Main/>
       
 
     </div>
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,33 +3,91 @@ import React from 'react'
 const Main = () => {
   return (
     <div className='wrapper'>
+      {/* for the icon */}
+      {/* <span></span> */}
         <div className='form-box login'>
           <h2>Login</h2>
           <form action='#'>
             <div className='input-box'>
               <span className='icon'></span>
-              <input type='email' required/>
+            <input
+              placeholder='Enter your Email Address'
+              type='email' required />
               <label>Email:</label>
             </div>
             <div className='input-box'>
               <span className='icon'></span>
-              <input type='password' required/>
-              <label>Password:</label>
+            <input
+              placeholder='Pls include your password required'
+              type='password' required />
+            <label>Password:</label>
+            
             </div> <br></br>
             <div className='remember-forgot'>
                       <label>
                           <input type='checkbox' />
-                          Remember me
-                      </label>
-                      <a href='#'>Forgot Password?</a>
-                  </div>
-                  <button type='submit' className='btn'>Login</button>
+                          Remember me     
+                      </label>     
+                           <a href='#'> Forgot Password?</a>
+                  </div><br/>
+          <button type='submit' className='btn'>Login</button>
                   <div className='login-register'>
-                      <p>Don't have an account <a href='#' className='register-link'>Register</a></p>
+                      <p>Don't have an account <a href='#' className='register-link'>Register Here!!</a></p>
                   </div>
           </form>
-          </div>
       </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+      <div className='form-box register'>
+          <h2>Registration</h2>
+          <form action='#'>
+            <div className='input-box'>
+              <span className='icon'></span>
+            <input
+              placeholder='Enter your name'
+              type='text' required />
+              <label>Username:</label>
+            </div>
+            <div className='input-box'>
+              <span className='icon'></span>
+            <input
+              placeholder='Include your password required'
+              type='password' required />
+            <label>Password:</label>
+            
+          </div> <br></br><br></br>
+          <div className='remember-forgot'>
+                      <label>
+                          <input type='checkbox' />
+                            I agree to the terms & conditions.
+                      </label>     
+                          
+                  </div>
+          
+          <button type='submit' className='btn'>Register</button>
+                  <div className='login-register'>
+            <p>Already have an account <a href='#' className='login-link'>Login</a></p>
+            
+                  </div>
+          </form>
+      </div>
+    </div>
   )
 }
 
